01298b69222bcfa8ed98764b629276c9a8d5e11c
Kayky-Eduardo <kayky002a@gmail.com>
2025-03-17 22:36:50 -0300

    tentativas

--- treino/treino.php
+++ treino/treino.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,61 +9,42 @@
 </head>
 
 <form method="post">
-    <label for="nome">Nome:</label>
-    <input type="text", id="nome", name="nome" required><br><br>
-
-    <label for="idade">idade:</label>
-    <input type="text", id="idade", name="idade" required><br><br>
-    
-    <label for="peso">peso:</label>
-    <input type="text", id="peso", name="peso" required>
-
     <label for="escolha">Escolha: </label>
-    <input type="text", id="escolha", name="escolha" required>
+    <input type="text" id="escolha" name="escolha" required>
 
     <button type="submit">Enviar</button>
 </form>
+
+
 <?php
 
-$escolha = htmlspecialchars($_POST['escolha']);
+if (!isset($_SESSION['lista'])) {
+    $_SESSION['lista'] = [];
+}
 
-while ($escolha != 3) {
-echo "1. Cadastrar";
-echo "2. Mostrar";
-echo "3. sair";
+echo "<h4>1. Cadastrar</h4>";
+echo "<h4>2. Mostrar</h4>";
+echo "<h4>3. sair</h4>";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $escolha = isset($_POST['escolha']) ? intval($_POST['escolha']) : null;
 
     if ($escolha == 1) {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $nome = htmlspecialchars($_POST['nome']);
             $idade = htmlspecialchars($_POST['idade']);
             $peso = htmlspecialchars($_POST['peso']);
-            $lista = [$nome=>$idade, $peso];
+            $lista[$nome] = ["idade"=>$idade, "peso"=>$peso];
         }
     } elseif ($escolha == 2) {
         echo "Lista atual: ";
         foreach($lista as $x=>$y) {
             echo "$x: $y";
         } 
-    } elseif ($escolha == 4) {
+    } elseif ($escolha == 3) {
         echo "Saindo..";
     } else {
-        echo "Opção inválida!"
+        echo "Opção inválida!";
     }
 }
-/*
-
-    foreach ($lista as $x=>$y) {
-        echo "$x : $y";
-    }
-
-
-    if ($escolha == 'Não') {
-    echo "lista atual $lista";
-    echo "Quem deseja procurar?";
-    } else {
-        echo "continuando!";
-        echo $lista;
-    }
-}
- */   
 ?>
